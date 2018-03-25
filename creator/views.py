9b47456a78a34d9a7e6b4f4635dadc507fe1ed9d
Ankit-22 <ankitmani.t@somaiya.edu>
2018-03-25 17:26:08 +0530

    Add Generate Hash

--- creator/views.py
+++ creator/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 from .tasks import TestTask
 from django.core.mail import EmailMessage
+from records.models import Event, Attendee
+from records.utils import generate_event_url, get_event_by_url
+from .utils import generate_hash
 
 
 # Create your views here.
@@ -19,3 +22,14 @@ def test(request):
 	return HttpResponse("Done!")
 
 
+def make_cert(request, event_name):
+
+	if request.method == 'GET':
+
+		event_url = generate_event_url(event_name)
+
+		for attendee in get_event_by_url( event_url ).attendees.all():
+			# Now generate model
+			print(generate_hash(attendee, event_name, event_url))
+
+		return HttpResponse("Welcome! ")
