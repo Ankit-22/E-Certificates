0cd9adfe96bd46da138a79ac23b8972b65f72509
Ankit-22 <ankitmani.t@somaiya.edu>
2018-03-25 20:52:26 +0530

    Add Jinja Template

--- checker/views.py
+++ checker/views.py
@@ -24,4 +24,4 @@ def show_certificate(request, certificate_hash):
 	string_to_show = "Name: " + certificate.certificate_holder.first_name + " " + certificate.certificate_holder.middle_name + " " + certificate.certificate_holder.last_name
 	string_to_show = string_to_show + "<br>College: " + certificate.certificate_holder.college + "<br>Email: " + certificate.certificate_holder.email
 	string_to_show = string_to_show + "<br>Event Name: " + certificate.certificate_event.event_name
-	return HttpResponse(string_to_show)
+	return render(request, 'view_cert.html',{'title': certificate.certificate_event.event_name, 'event_name': certificate.certificate_event.event_name, 'name': certificate.certificate_holder.first_name + " " + certificate.certificate_holder.middle_name + " " + certificate.certificate_holder.last_name, 'email': certificate.certificate_holder.email, 'college': certificate.certificate_holder.college})
