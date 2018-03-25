9692f613889d98dc544721a0b70a4b1db14670ef
Ankit-22 <ankitmani.t@somaiya.edu>
2018-03-25 19:28:23 +0530

    Add Checker Tests

--- checker/views.py
+++ checker/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from creator.models import Certificate
+
 # Create your views here.
 
 def test(request):
 	return HttpResponse("Hello, World!")
+
+def show_certificate(request, certificate_hash):
+	certificate = ""
+	try:
+		certificate = Certificate.objects.get(certificate_identifier = certificate_hash)
+	except Certificate.DoesNotExist:
+		return HttpResponse("404 Not Found")
+	except Certificate.MultipleObjectsReturned:
+		print("This is impossible")
+		return HttpResponse("400 Bad Request")
+	except:
+		print("Don't expect this")
+	return HttpResponse(certificate.certificate_event.event_name)
